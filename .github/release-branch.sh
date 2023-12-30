75dae2fa31142e84ebcf2772a50d586945dce039
Carson Yang <yangchuansheng33@gmail.com>
2023-12-30 22:46:29 +0800

    Update release-branch.sh

--- .github/release-branch.sh
+++ .github/release-branch.sh
@@ -5,9 +5,9 @@ set -e -o pipefail
 mkdir -p release
 cd release
 git init
-git config --local user.email "github-action@users.noreply.github.com"
-git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/SagerNet/sing-geosite.git
+git config --local user.email "yangchuansheng33@gmail.com"
+git config --local user.name "yangchuansheng"
+git remote add origin https://yangchuansheng:$GITHUB_TOKEN@github.com/yangchuansheng/sing-geosite.git
 git branch -M release
 cp ../*.db ../*.sha256sum .
 git add .
