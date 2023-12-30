ba005be4cddba3fc6669228b882a5ac38642d061
Carson Yang <yangchuansheng33@gmail.com>
2023-12-30 22:44:14 +0800

    Update release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -6,7 +6,7 @@ cd rule-set
 git init
 git config --local user.email "yangchuansheng33@gmail.com"
 git config --local user.name "yangchuansheng"
-git remote add origin https://yangchuansheng:$GH_PAT@github.com/yangchuansheng/sing-geosite.git
+git remote add origin https://yangchuansheng:$GITHUB_TOKEN@github.com/yangchuansheng/sing-geosite.git
 git branch -M rule-set
 git add .
 git commit -m "Update rule-set"
